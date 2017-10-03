df71fef912030a52217aa0f12a534e4905c65a82
Kirsten Land <kirstyland29@gmail.com>
2017-10-03 19:35:09 +0100

    Make newmap welcome screen, no design yet

--- mathsmap/new_map.py
+++ mathsmap/new_map.py
@@ -33,8 +33,20 @@ class NewMap(Wizard):
         Sets up first screen of wizard
         """
         self.clear()
-        self.title = tk.Label(self.top, text="Let's make a new map!")
-        self.title.pack()
+        self.title = tk.Label(self.top, text="Let's make a new map!", font=(None, 20))
+        self.title.grid(row=0, column=0, columnspan=2)
+        self.text = tk.Label(self.top,
+                             text=("When do you need to make your mathsmap? " +
+                                   "Is it right now, in a rush before exams, " +
+                                   "or over time, while you attend lectures and learn?"),
+                             wraplength=400,
+                            )
+        self.text.grid(row=1, column=0, columnspan=2)
+        buttons_needed = [("Right now", 0),
+                          ("Over time", 1)]
+        for text, column in buttons_needed:
+            button = tk.Button(self.top, text=text, width=20, height=5)
+            button.grid(row=2, column=column, pady=5)
 
     def clear(self):
         """
