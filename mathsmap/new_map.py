034c141ca739b272cfe2efb53b63422ec78369d4
Kirsten Land <kirstyland29@gmail.com>
2017-10-03 20:02:28 +0100

    Colour welcome screen of new map

--- mathsmap/new_map.py
+++ mathsmap/new_map.py
@@ -3,6 +3,7 @@ Contains classes controlling wizards for making new maps
 """
 
 import tkinter as tk
+import mathsmap.colours as colours
 
 class Wizard:
     """
@@ -33,19 +34,25 @@ class NewMap(Wizard):
         Sets up first screen of wizard
         """
         self.clear()
-        self.title = tk.Label(self.top, text="Let's make a new map!", font=(None, 20))
+        self.scheme = colours.BLUESCHEME
+        self.background = self.scheme.lighter
+        self.top.configure(background=self.background)
+        self.title = tk.Label(self.top, text="Let's make a new map!", font=(None, 20),
+                              background=self.background)
         self.title.grid(row=0, column=0, columnspan=2)
         self.text = tk.Label(self.top,
                              text=("When do you need to make your mathsmap? " +
-                                   "Is it right now, in a rush before exams, " +
+                                   "Is it right now, possibly in a rush before exams, " +
                                    "or over time, while you attend lectures and learn?"),
-                             wraplength=400,
+                             wraplength=400, background=self.background
                             )
         self.text.grid(row=1, column=0, columnspan=2)
-        buttons_needed = [("Right now", 0),
-                          ("Over time", 1)]
+        buttons_needed = [("Right now!", 0),
+                          ("Over time!", 1)]
         for text, column in buttons_needed:
-            button = tk.Button(self.top, text=text, width=20, height=5)
+            button = tk.Button(self.top, text=text, width=15, height=3, 
+                               background=self.scheme.darkest, activebackground=self.scheme.darker,
+                               foreground="white", font=(None, 15))
             button.grid(row=2, column=column, pady=5)
 
     def clear(self):
